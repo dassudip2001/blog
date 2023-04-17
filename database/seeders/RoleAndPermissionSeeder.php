069f34ae80eae56b0a3edb0add01e57b50042cc9
Sudip Das <sudip5428@gmail.com>
2023-04-17 20:20:09 +0530

    code commit for controller add

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Permission::create(['name' => 'create-users']);
+        Permission::create(['name' => 'edit-users']);
+        Permission::create(['name' => 'delete-users']);
+
+        Permission::create(['name' => 'create-blog-posts']);
+        Permission::create(['name' => 'edit-blog-posts']);
+        Permission::create(['name' => 'delete-blog-posts']);
+
+        $adminRole = Role::create(['name' => 'Admin']);
+        $editorRole = Role::create(['name' => 'Editor']);
+
+        $adminRole->givePermissionTo([
+            'create-users',
+            'edit-users',
+            'delete-users',
+            'create-blog-posts',
+            'edit-blog-posts',
+            'delete-blog-posts',
+        ]);
+
+        $editorRole->givePermissionTo([
+            'create-blog-posts',
+            'edit-blog-posts',
+            'delete-blog-posts',
+        ]);
+
+        $user = User::first();
+
+        $user->assignRole('Admin');
+
+        $role = Role::findByName('Admin');
+
+        $role->givePermissionTo('edit-users');
+    }
+}
