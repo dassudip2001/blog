dc94b92358b7c2ab28a7f25222dc9a3988275cfd
Sudip Das <sudip5428@gmail.com>
2023-04-18 20:35:18 +0530

    code commit for view page add

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Blog Create Page') }}
+        </h2>
+    </x-slot>
+
+    {{-- start  --}}
+    {{-- end --}}
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,11 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     {{-- category --}}
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('category.index')" :active="request()->routeIs('category.index')">
                         {{ __('Category') }}
                     </x-nav-link>
                     {{-- blog --}}
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('blog.index')" :active="request()->routeIs('blog.index')">
                         {{ __('Blog') }}
                     </x-nav-link>
                 </div>
